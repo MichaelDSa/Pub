ab9c4aa6ae869d6a4ebe36d90e83f3b39dd9d0e9
MichaelDSa <dsa.michael@gmail.com>
2024-06-21 18:32:00 -0400

    From MacOS

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // from Linux:
         System.out.println("From Linux: Hello world!");
+        //from Mac
+        System.out.println("From MacOS: Hello world!");
     }
 }
